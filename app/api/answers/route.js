b9a30a959b6cc5e7f066618a297b98c4265499a0
Kakrote <anshul.pundir111@gmail.com1>
2025-06-04 13:04:59 +0530

    added answers routes for update delete and fetching answers to a question

--- app/api/answers/route.js
+++ app/api/answers/route.js
@@ -1,4 +1,4 @@
-import { answerQuestion } from "@/controllers/answerController";
+import { answerQuestion ,updateAnswer,deleteAnswer,getAnswersByQuestionId } from "@/controllers/answerController";
 import { verifyAuth } from "@/middleware/auth";
 import { use } from "react";
 
@@ -21,4 +21,49 @@ export async function POST(req) {
         console.error("Error creating answer:", error);
         return new Response(JSON.stringify({ message: "Something went wrong." }), { status: 500 });
     }
+}
+
+export async function PUT(req) {
+    const user=await verifyAuth(req);
+    if(!user) return new Response(JSON.stringify({message:"User unauthorized"}), {status:400});
+    try{
+        const body=await req.json();
+        const {answerId,newContent}=body;
+        const result=await updateAnswer({
+            answerId,
+            authorId:user.userId,
+            newContent:newContent
+        });
+        return new Response(JSON.stringify({result}))
+    }
+    catch(error){
+        console.log(error)
+        return new Response(JSON.stringify({message:"Something went wrong"}),{status:500})
+    }
+}
+
+export async function DELETE(req) {
+    const user = await verifyAuth(req);
+    if (!user) return new Response(JSON.stringify({ message: "Unauthorized" }), { status: 401 });
+
+    const { answerId} = await req.json();
+    console.log("answerId: ",answerId)
+    console.log("authorId:",user.userId)
+    const result = await deleteAnswer({ answerId, authorId:user.userId });
+
+    return new Response(JSON.stringify(result), { status: result.status });
+}
+
+
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const questionId = searchParams.get("questionId");
+
+  const result = await getAnswersByQuestionId(questionId);
+
+  return new Response(JSON.stringify(result), {
+    status: result.status,
+    headers: { "Content-Type": "application/json" },
+  });
 }
--- controllers/answerController.js
+++ controllers/answerController.js
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 
+// write an answer 
 export const answerQuestion=async({questionslug,content,authorId})=>{
     if(!questionslug||!content) return {status:400,message:"Filds are required !"};
     try{
@@ -25,3 +26,98 @@ export const answerQuestion=async({questionslug,content,authorId})=>{
         return {status:500,message:"Server error !"}
     }
 }
+
+// update an answer
+
+export const updateAnswer=async({answerId,authorId,newContent})=>{
+    if(!answerId||!authorId||!newContent) return {status:400,message:"Filds are required !"};
+    try{
+        const existing=await prisma.answer.findUnique({
+            where:{id:answerId}
+        })
+        if(!existing||existing.authorId!==authorId) return {status:400,message:"unauthorized or answer not found !"};
+        const update=await prisma.answer.update({
+            where:{id:answerId},
+            data:{content:newContent},
+            include:{
+                author:{
+                    select:{
+                        name:true,
+                        email:true
+                    }
+                }
+            }
+        })
+
+        return {status:200,message:"Answer is updated successfully",update};
+    }
+    catch(error){
+        console.log(error);
+        return {status:500,message:"Server error !"};
+    }
+}
+
+// delete the answer
+
+export const deleteAnswer=async({answerId,authorId})=>{
+    console.log("answerId: ",answerId)
+    // console.log("authorId: ",authorId)
+    if(!answerId||!authorId) return {status:400,message:"Filds are required !"}
+    try{
+        const existing=await prisma.answer.findUnique({
+            where:{id:answerId}
+        })
+        if(!existing||existing.authorId!==authorId) return{status:400,message:"unauthorized or answer not found !"};
+        await prisma.answer.delete({
+            where:{id:answerId}
+        })
+        return {status:200,message:'Answer deleted successfully !'};
+    }
+    catch(error){
+        console.log(error);
+        return {status:500,message:'server error'}
+    }
+}
+
+// fething all the answers for an question
+
+export const getAnswersByQuestionId = async (questionId) => {
+  if (!questionId) return { status: 400, message: "Question ID is required" };
+
+  try {
+    const answers = await prisma.answer.findMany({
+      where: { questionId },
+      include: {
+        question:{
+            select:{
+                title:true
+            }
+        },
+        author: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+        comments: {
+          select: {
+            content: true,
+            createdAt: true,
+            author: {
+              select: { name: true },
+            },
+          },
+        },
+        votes: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return { status: 200, answers };
+  } catch (error) {
+    console.error(error);
+    return { status: 500, message: "Server error!" };
+  }
+};
