35c10203172043e5e3749a083b4e4e5341986a0a
abhay2133 <abhaybishthestudent@gmail.com>
2025-07-26 00:13:08 +0530

    feat: register userContent reducer in Redux store
    - Import userContentReducer from userContentSlice
    - Add userContent to rootReducer configuration
    - Enable state management for user-specific content

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import questionReducer from './slices/questionSlice';
+import userContentReducer from './slices/userContentSlice';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from '@reduxjs/toolkit';
@@ -13,7 +14,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  question:questionReducer
+  question: questionReducer,
+  userContent: userContentReducer
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
